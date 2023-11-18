8f8cdfb6d62787551c431ea6e0997613fc180548
kevinzzzzz <jie736805161@qq.com>
2023-11-18 22:59:07 +0800

    fix: 完善vue模板的eslint规则

--- kevin-template/vue-ts-vite/.eslintrc.cjs
+++ kevin-template/vue-ts-vite/.eslintrc.cjs
@@ -26,15 +26,25 @@ module.exports = {
     "no-alert": "error", // 禁止使用alert
     "no-empty": "error",// 是否块语句中的内容不能为空
     "no-func-assign": "error",// 是否禁止重复的函数声明
+    "no-dupe-keys": 2, //在创建对象字面量时不允许键重复
     "no-const-assign": "error",// 是否禁止修改const声明的变量
     "no-var": "error", // 是否不能使用var声明变量
     'vue/multi-word-component-names': 'off', // 关闭组件命名规则
     'vue/no-v-model-argument': 'off', // 禁止v-model带参
+    'vue/no-multiple-template-root': 0, // 禁止运行只有一个root节点
     "@typescript-eslint/no-explicit-any": "off", // 是否禁用 any
     "@typescript-eslint/no-empty-function": "error", // 是否禁用 空函数
     "@typescript-eslint/ban-types": "off", // 是否禁止特定类型（抽象定义）
     "@typescript-eslint/no-unused-vars": "off", // 是否禁止定义变量未使用
     "@typescript-eslint/prefer-const": "off", // 是否禁止偏好const
-    "@typescript-eslint/no-var-requires": 0 // 是否禁止使用require定义依赖
+    "@typescript-eslint/no-var-requires": 0, // 是否禁止使用require定义依赖
+    "vue/max-attributes-per-line": ["error", { // vue元素单行属性控制
+      "singleline": {
+        "max": 3
+      },      
+      "multiline": {
+        "max": 2
+      }
+    }]
   },
 }
