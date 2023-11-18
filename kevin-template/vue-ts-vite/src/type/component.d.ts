0b938ad853254ef47b824a3cdfb0c089aca0551a
kevinzzzzz <jie736805161@qq.com>
2023-11-18 23:01:07 +0800

    fix: 完善vue模板的eslint规则

---
+++ kevin-template/vue-ts-vite/src/type/component.d.ts
@@ -0,0 +1,8 @@
+import '@vue/runtime-core'
+import components from '@kevinPro/components';
+export {}
+declare module "@vue/runtime-core" {
+  export interface GlobalComponents {
+  }
+}
+export {};
